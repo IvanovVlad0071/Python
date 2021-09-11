c3e013c5dd833cbddd69bd3a8e716a488aa43be6
Ivanov_Vlad <ivanov.vlad.0071@gmail.com>
2021-09-11 15:28:31 +0300

    dz_python

---
+++ dz_1_python.py
@@ -0,0 +1,59 @@
+# Создать переменную типа string
+my_string = 'Hello Python '
+print(my_string)
+print(type(my_string))
+# Создать переменную типа integer
+param = 12
+print(param)
+print(type(param))
+# Создать переменную типа float
+param_1 = 3.14
+print(param_1)
+print(type(param_1))
+# создать переменную типа bytes
+b = bytes('john travolta','utf-8')
+print(b)
+print(type(b))
+# создать переменную типа list
+spisok = ['cat','dog','bird']
+print('cat','dog','bird')
+print(type(spisok))
+# создать переменную типа tuple
+kortezh = ('12','dragon','777',)
+print(kortezh)
+print(type(kortezh))
+# создать переменную типа set
+Set = {234234,'john wick','bond007'}
+print(Set)
+print(type(Set))
+# создать переменную типа frozen set
+set_1 = {12,23,34,34}
+fz = frozenset(set_1)
+print(fz)
+print(type(fz))
+# создать переменную тип dict
+word ={'word_1': 1, 'word_2': 2}
+print(word)
+print(type(word))
+# Создать 2 переменные String, создать переменную в которой суммируете эти переменные. Вывести в консоль.
+parametr = 'James '
+parametr_1 = 'Bond'
+parametr_2 =(parametr+parametr_1)
+print(parametr_2)
+# Создать 2 переменные Integer, создать переменную в которой суммируете эти переменные. Вывести в консоль.
+znak_1 = 10
+znak_2 = 35
+znak_3 = (znak_1 + znak_2)
+print(znak_3)
+# Создать переменную в которой Разделите эти переменные Int. Вывести в консоль.
+znak_4 = (znak_2 / znak_1)
+print(znak_4)
+# Создать переменную в которой Умножите эти переменные Int. Вывести в консоль.
+znak_5 = (znak_1 * znak_2)
+print(znak_5)
+# Создать переменную в которой Разделите без остатка эти переменные Int. Вывести в консоль.
+znak_6 = (znak_2 // znak_1)
+print(znak_6)
+# Создать переменную в которой надо присвоить остаток от деления этих переменные Int. Вывести в консоль
+znak_7 = (znak_2 % znak_1)
+print(znak_7)
